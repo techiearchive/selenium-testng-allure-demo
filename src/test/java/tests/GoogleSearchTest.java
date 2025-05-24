114cf463b68e4f7b04324074d18428281ea310d4
techieaem <techiearchive8@gmail.com>
2025-05-25 02:36:48 +0530

    Updated files to fix the build

--- src/test/java/tests/GoogleSearchTest.java
+++ src/test/java/tests/GoogleSearchTest.java
@@ -8,14 +8,29 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class GoogleSearchTest {
     WebDriver driver;
 
-    @BeforeClass
-    public void setup() {
-        driver = new ChromeDriver();
-    }
+
+@BeforeClass
+public void setup() throws Exception {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+
+    // Optional: set a unique temp user-data-dir
+    Path tempProfile = Files.createTempDirectory("chrome-profile");
+    options.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
+
+    driver = new ChromeDriver(options);
+}
+
+
 
     @Test
     public void testGoogleSearch() {
