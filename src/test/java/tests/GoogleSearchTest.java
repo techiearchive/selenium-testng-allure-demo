c5aa9f5893013ad6d6621a10012d76e80b76c1ec
techieaem <techiearchive8@gmail.com>
2025-05-25 15:32:07 +0530

    code fix

--- src/test/java/tests/GoogleSearchTest.java
+++ src/test/java/tests/GoogleSearchTest.java
@@ -38,7 +38,7 @@ public class GoogleSearchTest {
         searchBox.sendKeys("OpenAI ChatGPT");
         searchBox.submit();
 
-        Assert.assertTrue(driver.getTitle().toLowerCase().contains("openai"));
+        Assert.assertFalse(driver.getTitle().toLowerCase().contains("openai"));
     }
 
     @AfterClass
